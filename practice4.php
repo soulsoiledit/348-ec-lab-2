19d52619c976a301c77b672189bffd5d3224227a
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-10-28 00:40:35 -0500

    Add practice 4 page

---
+++ practice4.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<pre>
+<?php 
+
+$var = $_GET["num"]; 
+
+$padding = strlen(strval($var * $var));
+
+echo str_repeat(" ", $padding + 1);
+for ($x = 1; $x <= $var; $x++) {
+  printf("%*d ", $padding, $x);
+}
+echo "<br>";
+
+
+for ($x = 1; $x <= $var; $x++) {
+  printf("%*d ", $padding, $x);
+  for ($y = 1; $y <= $var; $y++) {
+    $mul = $x * $y;
+    printf("%*d ", $padding, $mul);
+  }
+  echo "<br>";
+}
+
+?><br>
+</pre>
+
+</body>
+</html> 
