b148d65936c986717df7686d5c85ae993b4ee11d
soulsoiledit <30585327+soulsoiledit@users.noreply.github.com>
2023-10-28 01:15:15 -0500

    Fix php indentation

--- php/practice4.php
+++ php/practice4.php
@@ -1,30 +1,30 @@
 <html>
   <body>
     <pre>
-      <?php 
+<?php
 
-      $var = $_GET["num"]; 
+$var = $_GET["num"];
 
-      $padding = strlen(strval($var * $var));
+$padding = strlen(strval($var * $var));
 
-      echo str_repeat(" ", $padding + 1);
-      for ($x = 1; $x <= $var; $x++) {
-      printf("%*d ", $padding, $x);
-      }
-      echo "<br>";
+echo str_repeat(" ", $padding + 1);
+for ($x = 1; $x <= $var; $x++) {
+  printf("%*d ", $padding, $x);
+}
+echo "<br>";
 
 
-      for ($x = 1; $x <= $var; $x++) {
-      printf("%*d ", $padding, $x);
-      for ($y = 1; $y <= $var; $y++) {
-      $mul = $x * $y;
-      printf("%*d ", $padding, $mul);
-      }
-      echo "<br>";
-      }
+for ($x = 1; $x <= $var; $x++) {
+  printf("%*d ", $padding, $x);
+  for ($y = 1; $y <= $var; $y++) {
+    $mul = $x * $y;
+    printf("%*d ", $padding, $mul);
+  }
+  echo "<br>";
+}
 
-      ?><br>
+?><br>
     </pre>
 
   </body>
-</html> 
+</html>
